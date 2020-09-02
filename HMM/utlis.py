179af8892dab32fddf64bc13249e9a2f9a2c1d46
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2020-09-02 17:55:10 +0200

    matrix multiplication

---
+++ HMM/utlis.py
@@ -0,0 +1,14 @@
+def matrix_mulitplication(a: list, b: list) -> list:
+    """Matrix multiplication.
+
+    Arguments:
+        a: Matrix with dimension n, m.
+        b: Matrix with dimension m, k.
+
+    Returns:
+        Matrix with shape n, k.
+    """
+    return [
+        [sum(ii * jj for ii, jj in zip(i, j)) for j in list(map(list, zip(*b)))]
+        for i in a
+    ]
