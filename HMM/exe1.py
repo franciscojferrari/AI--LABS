8081cb8059320bc2f31e40d5ebfda87925c73396
Francisco Ferrari <33513662+franciscojferrari@users.noreply.github.com>
2020-09-03 00:07:24 +0200

    V1

--- HMM/exe1.py
+++ HMM/exe1.py
@@ -1,6 +1,7 @@
 
-f = open("sample_00.in", "r")
-text = f.read()
+import sys
+
+
 
 def parse_input(input_value : str) -> list:
     
@@ -13,7 +14,7 @@ def parse_input(input_value : str) -> list:
         list: A list with the matrixes [A, B, pi]
     """
 
-    matrixes = ()
+    matrixes = []
     # print(input_value.splitlines())
     for rows in input_value.splitlines():
         values = rows.split(" ")
@@ -22,3 +23,39 @@ def parse_input(input_value : str) -> list:
         
         matrixes.append([matrix_values [i : i+dimensions] for i in range(0, len(matrix_values), dimensions)])
     return matrixes
+
+
+def matrix_mulitplication(a: list, b: list) -> list:
+    """Matrix multiplication.
+
+    Arguments:
+        a: Matrix with dimension n, m.
+        b: Matrix with dimension m, k.
+
+    Returns:
+        Matrix with shape n, k.
+    """
+    return [
+        [sum(ii * jj for ii, jj in zip(i, j)) for j in list(map(list, zip(*b)))]
+        for i in a
+    ]
+
+
+def parse_output(matrix:list)->str:
+    rows = len(matrix)
+    columns = len(matrix[0])
+    list = [rows, columns] + [item for row in matrix for item in row]
+
+    print (' '.join(map(str, list)))
+
+
+def main():
+    # print command line arguments
+    file_content = ""
+    for text in sys.stdin:
+       file_content+=text
+    A, B, pi = parse_input(file_content)
+    parse_output(matrix_mulitplication(matrix_mulitplication(pi, A), B))
+
+if __name__ == "__main__":
+    main()
