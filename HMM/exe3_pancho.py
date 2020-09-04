f69de31dcd77e2f3d20f20779a9545b4d7aee678
Francisco Ferrari <33513662+franciscojferrari@users.noreply.github.com>
2020-09-04 09:16:44 +0200

    improved and with some comments

--- HMM/exe3_pancho.py
+++ HMM/exe3_pancho.py
@@ -3,17 +3,32 @@ import itertools
 RECURSIVE = True
 
 
-def elem_wise_product(vector_a, vector_b):
+def elem_wise_product(vector_a:list, vector_b:list)->list:
+    """Element-wise product between 2 vectors
+
+    Args:
+        vector_a (list): Vector A
+        vector_b (list): Vector C
+
+    Returns:
+        list: New vector with the result of the element-wise multiplication between A and B
+    """
     return [(element_a * element_b) for element_a , element_b in zip(vector_a, vector_b)]
 
-def elem_wise_product_matrix(matrix_a, matrix_b):
-    matrix_a_n = len(matrix_a[0])
-    flat_a = ([i for row in matrix_a for i in row])
-    flat_b = ([i for row in matrix_b for i in row])
-    result = elem_wise_product(flat_a, flat_b)
+def elem_wise_product_matrix(matrix_a:list, matrix_b:list)->list:
+    """Element-wise product between 2 matrices
 
-    return [result[i : i + matrix_a_n] for i in range(0, len(result), matrix_a_n)] 
+    Args:
+        matrix_a (list): Matrix A (dimension n, m)
+        matrix_b (list): Matrix B (dimension n, m)
 
+    Returns:
+        list:  Element-wise multiplication between A and B (dimension n, m)
+    """
+    n_columns = len(matrix_a[0])
+    result = [a * b for row_a, row_b in zip(matrix_a, matrix_b) for a, b in zip(row_a, row_b)]
+
+    return [result[i : i + n_columns] for i in range(0, len(result), n_columns)] 
 
 def matrix_mulitplication(a: list, b: list) -> list:
     """Matrix multiplication.
@@ -37,7 +52,7 @@ def parse_input(input_value: str) -> list:
         input_value (str): input string
 
     Returns:
-        list: A list with the matrixes [A, B, pi]
+        list: A list with the matrixes [A, B, pi, O]
     """
     matrixes = []
     for idx, rows in enumerate(input_value.splitlines()):
@@ -58,10 +73,30 @@ def parse_input(input_value: str) -> list:
     return matrixes
 
 def T(matrix:list)->list:
+    """Transpose of matrix
+
+    Args:
+        matrix (list): Matrix dimension n, m
+
+    Returns:
+        list: Transponsed matrix of dimension m, n
+    """
     return list(map(list, zip(*matrix)))
 
+def foward_algorithm_recursive (A:List[List], B:List[List], pi:list, alpha:list, O:list, first_interation:bool = True) -> float:
+    """Foward Algo
 
-def foward_algorithm_recursive (A:list, B:list, pi:list, alpha:list, O:list, first_interation:bool = True) -> float:
+    Args:
+        A (List[List]): Transition Matrix
+        B (List[List]): Emission / Output probability matrix
+        pi (list): Initial state vector
+        alpha (list): vector that represents the probability of being in state j after seeing the first t observations,
+        O (list): vector of emissions sequences itself
+        first_interation (bool, optional): states if the function is the first time is running. Defaults to True.
+
+    Returns:
+        float: Sum of Alpha
+    """
     if len(O) > 0:
         if first_interation:
             alpha = elem_wise_product(pi[0], T(B)[O.pop(0)])
@@ -71,7 +106,19 @@ def foward_algorithm_recursive (A:list, B:list, pi:list, alpha:list, O:list, fir
             return foward_algorithm_recursive(A, B, pi, alpha, O, False)
     print(sum(alpha))
 
-def foward_algorithm (A:list, B:list, pi:list, O:list) -> float:
+def foward_algorithm (A:List[List], B:List[List], pi:list, O:list) -> float:
+    """[summary]
+
+    Args:
+        A (List[List]): Transition Matrix
+        B (List[List]): Emission / Output probability matrix
+        pi (list): Initial state vector
+        alpha (list): vector that represents the probability of being in state j after seeing the first t observations,
+        O (list): vector of emissions sequences itself
+
+    Returns:
+        float: [description]
+    """
     for index, emission in enumerate(O):
         if index == 0:
             alpha = elem_wise_product(pi[0], T(B)[emission])
@@ -79,53 +126,80 @@ def foward_algorithm (A:list, B:list, pi:list, O:list) -> float:
             alpha =elem_wise_product( matrix_mulitplication([alpha], A)[0], T(B)[emission])
     print(sum(alpha))
 
-def viterbi_algorithm(A:list, B:list, pi:list, O:list) -> float:
+def viterbi_algorithm(A:list, B:list, pi:list, O:list) -> list :
+    """[summary]
+
+    Args:
+        A (List[List]): Transition Matrix
+        B (List[List]): Emission / Output probability matrix
+        pi (list): Initial state vector
+        alpha (list): vector that represents the probability of being in state j after seeing the first t observations,
+        O (list): vector of emissions sequences itself
+
+
+    Returns:
+        list: [description]
+        list: [description]
+    """
+
     theta_list = []
     theta_idx_list = []
-    for index, emission in enumerate(O):
-        if index == 0:
-            theta_i = elem_wise_product(pi[0], T(B)[emission])
-        else:
-            theta_m = [theta_i for _ in theta_i]
-            B_m_t = T([T(B)[emission] for _ in T(B)[emission]])
+    B_T = T(B)
+    A_T = T(A)
 
-            result =  elem_wise_product_matrix(elem_wise_product_matrix(T(A),theta_m), B_m_t)
+    for index, observation_t in enumerate(O):
+        if index == 0:
+            theta_i = elem_wise_product(pi[0], T(B)[observation_t])
+        else:
+            theta_m = [theta_i for _ in theta_i] #Copy vector theta_i to create a new matrix of size n, n
+            obs_t_m_T = T([B_T[observation_t] for _ in theta_i]) #Get the overservation (vector) --> copy it to create matrix of size theta --> transpose it to get same values per row
+                #[0.6, 0.1, 0.0, 0.0] ->[[0.6, 0.1, 0.0, 0.0], [0.6, 0.1, 0.0, 0.0], [0.6, 0.1, 0.0, 0.0], [0.6, 0.1, 0.0, 0.0]] -->[[0.6, 0.6, 0.6, 0.6], [0.1, 0.1, 0.1, 0.1], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]]
+            result =  elem_wise_product_matrix( elem_wise_product_matrix(A_T, theta_m), obs_t_m_T )
             #find the max
-            theta_i = ([max(row) for row in result])
-            theta_idx = [row.index( max(row)) for row in result]
+            theta_i = ([max(row) for row in result]) #find theta by comuting the argmax between each element in the rows of the result matrix 
+            theta_idx = [row.index( max(row)) for row in result] #find theta idx by comuting the argmax between each element in the rows of the result matrix and geting the index
 
             theta_list.append(theta_i)
             theta_idx_list.append(theta_idx)
 
     return theta_list, theta_idx_list
 
+def viterbi_algorithm_optimal_sequence(theta_list:list, theta_idx_list:list, sequence:list = None)->list:
+    """[summary]
 
-def optimal_sequence(theta_list, theta_idx_list, sequence = None):
+    Args:
+        theta_list (list): [description]
+        theta_idx_list (list): [description]
+        sequence (list, optional): [description]. Defaults to None.
+
+    Returns:
+        list: [description]
+    """
     ##get the max
     if len(theta_idx_list) > 0:
         if not sequence:
             index_max_value = theta_list[-1].index( max(theta_list[-1]) )
             sequence = [index_max_value]
-            return optimal_sequence(theta_list, theta_idx_list, sequence)
+            return viterbi_algorithm_optimal_sequence(theta_list, theta_idx_list, sequence)
         else:
             # print(sequence)
             next_idx = theta_idx_list.pop()[sequence[-1]]
             sequence.append(next_idx)
-            return optimal_sequence(theta_list, theta_idx_list, sequence)
+            return viterbi_algorithm_optimal_sequence(theta_list, theta_idx_list, sequence)
     else:
-        return_sequence = [str(i) for i in sequence[::-1]]
-        return_sequence = " ".join(return_sequence)
-        return return_sequence
+        return sequence
     
-    
+def print_list(input_list:list)->str:
+    return_list = [str(i) for i in input_list[::-1]]
+    print( " ".join(return_list))
 
 def main():
     file_content = "".join([text for text in sys.stdin])
     A, B, pi, O = parse_input(file_content)
     theta_list, theta_idx_list = viterbi_algorithm(A, B, pi, O)
+    optimal_seq = viterbi_algorithm_optimal_sequence(theta_list, theta_idx_list)
     # print(theta_list, theta_idx_list)
-    print(optimal_sequence(theta_list, theta_idx_list))
-
+    print_list(optimal_seq)
 
 if __name__ == "__main__":
     main()
