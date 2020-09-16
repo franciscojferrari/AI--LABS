631cf73e6495aeee2f36949a5951b8aa2c32b5ca
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2020-09-16 13:19:27 +0200

    updated heuristic

--- minimax_assignment/ai_player.py
+++ minimax_assignment/ai_player.py
@@ -18,13 +18,13 @@ class MinMaxModel(object):
             if mini_max_result >= alpha:
                 alpha = mini_max_result
                 possible_values[child.move] = mini_max_result
-        if all(value == possible_values[0] for value in possible_values.values()):
-            return random.randint(0, 4)
+        # if all(value == possible_values[0] for value in possible_values.values()):
+        #     return random.randint(0, 4)
         return max(possible_values, key=possible_values.get)
 
     def minimax_algorith(self, node, depth, alpha = -math.inf, betta = math.inf):
-        if(depth == 0 or len(node.compute_and_get_children()) == 0):
-            ##Calculate Heuristics
+        if depth == 0 or len(node.compute_and_get_children()) == 0:
+            # Calculate Heuristics
             return self.get_heuristic(node)
         if node.state.player == 0:
             maxEval = -math.inf
@@ -32,7 +32,7 @@ class MinMaxModel(object):
                  maxEval = max(maxEval, self.minimax_algorith(child, depth -1, alpha, betta))
                  alpha = max(alpha, maxEval)
                  if alpha >= betta:
-                    ##Alpha betta prunning. We stop the interation on the children nodes
+                    # Alpha betta prunning. We stop the interation on the children nodes
                     break
             return maxEval
         else:
@@ -41,16 +41,16 @@ class MinMaxModel(object):
                 minEval = min(minEval, self.minimax_algorith(child, depth -1, alpha, betta))
                 betta = min(betta, minEval)
                 if betta <= alpha:
-                    ##Alpha betta prunning. We stop the interation on the children nodes
-                    #Alpha cut-off
-
+                    # Alpha betta prunning. We stop the interation on the children nodes
+                    # Alpha cut-off
                     break
             return minEval
 
     def get_heuristic(self, node):
         """
         - Distance of hook to closest fish
-        - Distance to high value fish
+        - Include score of the fish
+        - Include score heuristic
         :return:
         """
         node = node.state
@@ -62,12 +62,27 @@ class MinMaxModel(object):
         # print(f"hook positions: {hook_positions}")
         # print(f"fish positions: {fish_positions}")
 
+        # MAX PLAYER SCORE
         max_pos_x,  max_pos_y = hook_positions[0]
 
-        min_distance = math.inf
+        distance_max = math.inf
         for _, location in fish_positions.items():
             fish_x, fish_y = location
-            distance = math.sqrt((fish_x-max_pos_x)**2 + (fish_y-max_pos_y)**2)
-            min_distance = min(distance, min_distance)
+            dist = math.sqrt((fish_x-max_pos_x)**2 + (fish_y-max_pos_y)**2)
+            distance_max = min(dist, distance_max)
 
-        return min_distance
+        # print(f"closest fish for max player: {distance_max}")
+        # MIN PLAYER SCORE
+        max_pos_x, max_pos_y = hook_positions[1]
+
+        distance_min = math.inf
+        for _, location in fish_positions.items():
+            fish_x, fish_y = location
+            dist = math.sqrt((fish_x - max_pos_x) ** 2 + (fish_y - max_pos_y) ** 2)
+            distance_min = min(dist, distance_min)
+
+        # print(f"closest fish for min player: {distance_min}")
+        heuristic = distance_min - distance_max
+
+        return heuristic
+
